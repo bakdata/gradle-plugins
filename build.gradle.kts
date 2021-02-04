fe82bbc472b115160444c33b32a6972157196e9d
Torben Meyer <torben.meyer@bakdata.com>
2021-02-04 09:26:50 +0100

    Allow configurable timeouts and simplify local maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 buildscript {
-    repositories {
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
     dependencies {
         classpath("org.gradle.kotlin:plugins:1.2.0")
         classpath("com.gradle.publish:plugin-publish-plugin:0.10.1")
@@ -19,6 +15,9 @@ plugins {
 }
 
 allprojects {
+    // required for local self-publish
+    plugins.apply("maven-publish")
+
     repositories {
         maven(url = "https://plugins.gradle.org/m2/")
     }
@@ -57,34 +56,29 @@ subprojects {
 }
 
 // config for gradle plugin portal
-// doesn't support snapshot, so we add config only if release version
-if(!version.toString().endsWith("-SNAPSHOT")) {
-    subprojects.forEach { project ->
-        with(project) {
-            // com.gradle.plugin-publish depends on java-gradle-plugin, but it screws a bit this project
-            apply(plugin = "java-gradle-plugin")
-            apply(plugin = "com.gradle.plugin-publish")
-            project.afterEvaluate {
-                // java-gradle-plugin requires this block, but we already added the definitions in META-INF for unit testing...
-                configure<GradlePluginDevelopmentExtension> {
-                    plugins {
-                        create("${project.name.capitalize()}Plugin") {
-                            id = "com.bakdata.${project.name}"
-                            implementationClass = "com.bakdata.gradle.${project.name.capitalize()}Plugin"
-                            description = project.description
-                        }
-                    }
+subprojects.forEach { project ->
+    with(project) {
+        // com.gradle.plugin-publish depends on java-gradle-plugin, but it screws a bit this project
+        apply(plugin = "java-gradle-plugin")
+        apply(plugin = "com.gradle.plugin-publish")
+        // java-gradle-plugin requires this block, but we already added the definitions in META-INF for unit testing...
+        configure<GradlePluginDevelopmentExtension> {
+            plugins {
+                create("${project.name.capitalize()}Plugin") {
+                    id = "com.bakdata.${project.name}"
+                    implementationClass = "com.bakdata.gradle.${project.name.capitalize()}Plugin"
+                    description = project.description
                 }
-                // actual block of plugin portal config, need to be done on each subproject as the plugin does not support multi-module projects yet...
-                configure<com.gradle.publish.PluginBundleExtension> {
-                    website = "https://github.com/bakdata/gradle-plugins"
-                    vcsUrl = "https://github.com/bakdata/gradle-plugins"
-                    (plugins) {
-                        "${name.capitalize()}Plugin" {
-                            displayName = "Bakdata $name plugin"
-                            tags = listOf("bakdata", name)
-                        }
-                    }
+            }
+        }
+        // actual block of plugin portal config, need to be done on each subproject as the plugin does not support multi-module projects yet...
+        configure<com.gradle.publish.PluginBundleExtension> {
+            website = "https://github.com/bakdata/gradle-plugins"
+            vcsUrl = "https://github.com/bakdata/gradle-plugins"
+            (plugins) {
+                "${name.capitalize()}Plugin" {
+                    displayName = "Bakdata $name plugin"
+                    tags = listOf("bakdata", name)
                 }
             }
         }
