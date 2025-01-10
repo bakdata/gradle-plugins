841954a1167325aeb84ff0d81a7b27af33a3242f
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-01-10 15:09:20 +0100

    Fix subproject setup for sonar plugin (#54)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.bakdata.release") version "1.4.0"
     // eat your own dog food - apply the plugins to this plugin project
     id("com.bakdata.sonar") version "1.4.0"
-    id("com.bakdata.sonatype") version "1.4.0"
+    id("com.bakdata.sonatype") version "1.4.1"
     id("org.gradle.kotlin.kotlin-dsl") version "4.1.2" apply false
     id("com.gradle.plugin-publish") version "1.2.1" apply false
 }
