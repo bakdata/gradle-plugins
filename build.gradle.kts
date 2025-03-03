8fc5ba7b2f91a7f91cdf8ae06af1b466ca524139
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-03-03 11:27:22 +0100

    Revert changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,29 +58,18 @@ subprojects {
         apply(plugin = "com.gradle.plugin-publish")
     }
 
-    tasks.matching { it.name.startsWith("publishSonatypePublication") }.configureEach {
-        enabled = false
-    }
-
-    val pluginName = "${project.name.replaceFirstChar(::capitalize)}Plugin"
-    configure<GradlePluginDevelopmentExtension> {
-        website.set("https://github.com/bakdata/gradle-plugins")
-        vcsUrl.set("https://github.com/bakdata/gradle-plugins")
-        plugins {
-            create(pluginName) {
-                id = "com.bakdata.${project.name}"
-                implementationClass = "com.bakdata.gradle.${project.name.replaceFirstChar(::capitalize)}Plugin"
-                displayName = "Bakdata $name plugin"
-                tags = listOf("bakdata", name)
-            }
-        }
-    }
     // description is only ready after evaluation
     afterEvaluate {
         configure<GradlePluginDevelopmentExtension> {
+            website.set("https://github.com/bakdata/gradle-plugins")
+            vcsUrl.set("https://github.com/bakdata/gradle-plugins")
             plugins {
-                getByName(pluginName) {
+                create("${project.name.replaceFirstChar(::capitalize)}Plugin") {
+                    id = "com.bakdata.${project.name}"
+                    implementationClass = "com.bakdata.gradle.${project.name.replaceFirstChar(::capitalize)}Plugin"
                     description = project.description
+                    displayName = "Bakdata $name plugin"
+                    tags = listOf("bakdata", name)
                 }
             }
         }
