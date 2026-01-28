d8e7e73c03ae477d25d73a99e08fbe8f51ad0518
Philipp Schirmer <philipp.schirmer@bakdata.com>
2026-01-28 13:51:24 +0100

    Upgrade to Gradle 9

--- sonar/src/test/kotlin/com/bakdata/gradle/SonarPluginIT.kt
+++ sonar/src/test/kotlin/com/bakdata/gradle/SonarPluginIT.kt
@@ -87,8 +87,8 @@ internal class SonarPluginIT {
                     mavenCentral()
                 }
                 dependencies {
-                testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-                testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+                    "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+                    "testImplementation"("org.junit.jupiter:junit-jupiter:5.13.4")
                 }
                 tasks.withType<Test> {
                     useJUnitPlatform()
@@ -142,8 +142,8 @@ internal class SonarPluginIT {
                     mavenCentral()
                 }
                 dependencies {
-                testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-                testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+                    "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+                    "testImplementation"("org.junit.jupiter:junit-jupiter:5.13.4")
                 }
                 tasks.withType<Test> {
                     useJUnitPlatform()
