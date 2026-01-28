d3c779f2eb83d2130229efb88deda061f4ab682d
Philipp Schirmer <philipp.schirmer@bakdata.com>
2026-01-28 21:14:14 +0100

    WIP

--- sonatype/src/main/kotlin/com/bakdata/gradle/SonatypePlugin.kt
+++ sonatype/src/main/kotlin/com/bakdata/gradle/SonatypePlugin.kt
@@ -251,7 +251,7 @@ class SonatypePlugin : Plugin<Project> {
 
             tasks.register("sign") { dependsOn(tasks.withType<Sign>()) }
 
-            tasks.matching { it is AbstractPublishToMaven }.all { dependsOn(tasks.withType<Sign>()) }
+//            tasks.matching { it is AbstractPublishToMaven }.all { dependsOn(tasks.withType<Sign>()) }
         }
     }
 
